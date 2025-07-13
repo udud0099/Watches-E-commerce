2195ef09c5611636f4227536cbc2772ae8474ac5
udud0099 <udud0099@gmail.com>
2025-07-13 10:53:34 +0545

    https://www.youtube.com/watch?v=uEb1fVFcTg8 - 1h:36m - let's stop hear - we put data in mongodb

--- my-app/components/AddForm.tsx
+++ my-app/components/AddForm.tsx
@@ -1,9 +1,13 @@
+"use client";
 import { addAction } from "@/utils/addAction";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
 import toast from "react-hot-toast";
 
 const AddForm = () => {
   const router = useRouter();
+  const [imageURL, setImageURL] = useState("");
   async function clientAddAction(formData: FormData) {
     const { error, success } = await addAction(formData);
 
@@ -14,12 +18,29 @@ const AddForm = () => {
     if (success) {
       toast.success(success);
       router.push("/");
+      setImageURL("");
     }
   }
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const fileSize = file.size;
+
+      if (Math.round(fileSize / 1024) > 1024) {
+        toast.error("image grate than 1mb is not allowed.");
+      } else {
+        setImageURL(URL.createObjectURL(file));
+      }
+    }
+  };
   return (
     <div>
       <form action={clientAddAction}>
         <div className="bg-white text-black p-8 flex gap-4 flex-col">
+          {imageURL && (
+            <Image src={imageURL} width={200} height={200} alt="img" />
+          )}
           <div>
             <label htmlFor="">
               Product Image
@@ -27,6 +48,7 @@ const AddForm = () => {
                 type="file"
                 accept="image/*"
                 name="image"
+                onChange={handleImageChange}
                 className="p-4 border "
               />
             </label>
